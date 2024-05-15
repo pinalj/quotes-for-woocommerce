8fe393f02e965f94adaba250ef8e294d049e5da5
pinalj <pinalj1612@gmail.com>
2024-05-15 23:30:03 +0530

    Modified author name and links
    Modified the author name to TechnoVama and updated links to redirect to woocommerce.com

--- quotes-woocommerce.php
+++ quotes-woocommerce.php
@@ -3,12 +3,13 @@
  * Plugin Name: Quotes for WooCommerce
  * Description: This plugin allows you to convert your WooCommerce store into a quote only store. It will hide the prices for the products and not take any payment at Checkout. You can then setup prices for the items in the order and send a notification to the Customer.
  * Version: 2.2.1
- * Author: Pinal Shah
+ * Author: TechnoVama
  * Requires at least: 4.5
  * WC Requires at least: 4.0
  * WC tested up to: 8.7.0
  * Text Domain: quote-wc
  * Domain Path: /languages/
+ * Author URI: https://woocommerce.com/vendor/technovama/
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
